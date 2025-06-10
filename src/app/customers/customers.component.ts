6d599d2e3499833c5099e004b70441af725e29ad
MohammedBourhym <mohammedbourhym9@gmail.com>
2025-06-10 11:14:24 +0100

    customers component created

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CustomersService } from '../services/customers.service';
+import { Customer } from '../model/customer.model';
+
+@Component({
+  selector: 'app-customers',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './customers.component.html',
+  styleUrls: ['./customers.component.css']
+})
+export class CustomersComponent implements OnInit {
+  customers: Customer[] = [];
+  searchKeyword: string = '';
+
+  constructor(private customersService: CustomersService) {}
+
+  ngOnInit(): void {
+    this.loadCustomers();
+  }
+
+  loadCustomers(): void {
+    this.customersService.getAllCustomers().subscribe({
+      next: data => this.customers = data,
+      error: err => console.error('Error loading customers:', err)
+    });
+  }
+
+  searchCustomers(): void {
+    if (this.searchKeyword.trim()) {
+      this.customersService.searchCustomers(this.searchKeyword).subscribe({
+        next: data => this.customers = data,
+        error: err => console.error('Error searching customers:', err)
+      });
+    } else {
+      this.loadCustomers();
+    }
+  }
+
+  deleteCustomer(id: number): void {
+    if (confirm('Are you sure you want to delete this customer?')) {
+      this.customersService.deleteCustomer(id).subscribe({
+        next: () => this.loadCustomers(),
+        error: err => console.error('Error deleting customer:', err)
+      });
+    }
+  }
+}
